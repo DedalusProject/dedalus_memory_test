799754c93a5b3c53b4b455ceb2e4e5c213f3116f
Keaton J. Burns <keaton.burns@gmail.com>
2020-04-23 16:16:41 -0400

    Tweak run.sh

--- test_rbc/run.sh
+++ test_rbc/run.sh
@@ -1,14 +1,17 @@
 
-alias mpi_command="mpiexec"
+# Set custom mpi command if needed (e.g. with core count)
+mpi_command="mpiexec"
 
+# Loop over test directories
 for testdir in ./test*/
 do
     cd $testdir
-    mpi_command python3 ../simulation.py
+    $mpi_command python3 ../simulation.py
     python3 ../plot_memory.py memory.txt --output=memory_$(basename $testdir).pdf
     cd ..
 done
 
+# Consolidate plots
 rm -rf plots
 mkdir plots
 cp -f test*/memory*.pdf plots
